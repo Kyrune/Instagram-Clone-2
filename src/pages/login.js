2a3faadf190f992d4225660d22cd41a2f7079d62
Kyrune <jn-01@centurytel.net>
2021-05-18 12:49:08 -0500

    Add useState variables

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,15 @@
 import { useHistory } from "react-router";
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import FirebaseContext from '../context/firebase';
 
 export default function Login() {
     const history = useHistory();
     const { firebase } = useContext(FirebaseContext);
+
+    // States
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
     return <p>Login page</p>;
 }
