e2fd3c5295015497658b5d16317ea5e8b4822180
Kyrune <jn-01@centurytel.net>
2021-05-18 12:45:50 -0500

    Import useContext

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,10 @@
 import { useHistory } from "react-router";
+import { useContext } from 'react';
+import FirebaseContext from '../context/firebase';
 
 export default function Login() {
     const history = useHistory();
-    
+    const { firebase } = useContext(FirebaseContext);
+
     return <p>Login page</p>;
 }
