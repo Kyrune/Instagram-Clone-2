bccabb98b1c47fb3855101722613f6b9c22886c2
Kyrune <jn-01@centurytel.net>
2021-05-18 12:40:39 -0500

    Import lazy/suspect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Login = lazy(() => import ('./pages/login'));
 
 function App() {
   return (
     <Router>
-      
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path='/login' component={Login} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 }
