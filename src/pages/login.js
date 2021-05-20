4c6807855fc5c1ebead830dba4a5e299e36d438e
Kyrune <jn-01@centurytel.net>
2021-05-20 12:59:40 -0500

    Add iPhone login screen img

--- src/pages/login.js
+++ src/pages/login.js
@@ -24,7 +24,9 @@ export default function Login() {
 
     return (
         <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-            <p>Something</p>
+            <div className="flex w-3/5">
+                <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram" />
+            </div>
         </div>
     );
 }
