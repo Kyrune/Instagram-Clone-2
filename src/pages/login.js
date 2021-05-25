cf3bc69b5fd40414cf450f8f26b6126af5044e3f
Kyrune <jn-01@centurytel.net>
2021-05-25 10:21:38 -0500

    Add input values

--- src/pages/login.js
+++ src/pages/login.js
@@ -56,6 +56,7 @@ export default function Login() {
                             placeholder="Email address"
                             className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
                             onChange={({ target }) => setEmailAddress(target.value)}
+                            value={emailAddress}
                         />
                                             <input 
                             aria-label="Enter your password"
@@ -63,6 +64,7 @@ export default function Login() {
                             placeholder="Password"
                             className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
                             onChange={({ target }) => setPassword(target.value)}
+                            value={password}
                         />
                         <button 
                             disabled={isInvalid}
@@ -76,7 +78,7 @@ export default function Login() {
                 <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
                     <p className="text-sm">
                         Don't have an account?{` `}
-                        <Link to="/signup" className="font-bold text-bold text-blue-medium">
+                        <Link to={ROUTES.SIGN_UP} className="font-bold text-bold text-blue-medium">
                             Sign Up
                         </Link>
                     </p>
