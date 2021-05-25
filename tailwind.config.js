c17428233f9d35e7f9dbe74abbdcc855bc648ff2
Kyrune <jn-01@centurytel.net>
2021-05-25 09:35:53 -0500

    Add white & blue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     future: {
         removeDeprecatedGapUtilities: true
+    },
+    theme: {
+        colors: {
+            white: '#ffffff',
+            blue: {
+                medium: '#005c98'
+            }
+        }
     }
-}
+};
