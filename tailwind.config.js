d06425dcff161aed33efe531daadf2711b3b6e50
Kyrune <jn-01@centurytel.net>
2021-05-25 09:36:21 -0500

    Add black

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
             white: '#ffffff',
             blue: {
                 medium: '#005c98'
+            },
+            black: {
+                light: '#005c98',
+                faded: '#00000059'
             }
         }
     }
