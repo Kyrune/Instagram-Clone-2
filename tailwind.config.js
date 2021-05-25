485b2b9d556d1f64674c225901ce8b709bafcfb9
Kyrune <jn-01@centurytel.net>
2021-05-25 09:37:10 -0500

    Add gray

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,11 @@ module.exports = {
             black: {
                 light: '#005c98',
                 faded: '#00000059'
+            },
+            gray: {
+                base: '#616161',
+                background: '#fafafa',
+                primary: '#dbdbdb'
             }
         }
     }
