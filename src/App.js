53ca8602234ba5a22a3682c7b360b63f015c7f18
Kyrune <jn-01@centurytel.net>
2021-06-07 10:59:28 -0500

    Import UserContext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import useAuthListener from './hooks/use-auth-listener';
+import UserContext from './context/user';
 import * as ROUTES from './constants/routes';
 
 const Login = lazy(() => import ('./pages/login'));
@@ -10,17 +11,19 @@ const NotFound = lazy(() => import('./pages/not-found'));
 
 export default function App() {
   const { user } = useAuthListener();
-  
+
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} />
-          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
