a6bfb1b8b88bad8591db1df73f167864f1016280
Kyrune <jn-01@centurytel.net>
2021-06-07 11:01:32 -0500

    Import UserContext

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import FirebaseContext from '../context/firebase';
+import UserContext from '../context/user';
 
 export default function Header() {
     return (
