be11e1f10cdab0d97b7a011bf35e90ed5835c094
Kyrune <jn-01@centurytel.net>
2021-06-07 11:06:11 -0500

    Add firebase and user useContext variables

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,9 @@ import FirebaseContext from '../context/firebase';
 import UserContext from '../context/user';
 
 export default function Header() {
+    const { firebase } = useContext(FirebaseContext);
+    const { user } = useContext(UserContext);
+
     return (
         <p>I am a header!</p>
     );
