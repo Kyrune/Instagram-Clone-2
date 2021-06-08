2aa370385f3c503b3b154011b6a82506eaeafc2e
Kyrune <jn-01@centurytel.net>
2021-06-08 10:43:42 -0500

    Create default function useUser

--- src/hooks/use.user.js
+++ src/hooks/use.user.js
@@ -1,2 +1,8 @@
 import { useState, useEffect, useContext } from 'react'
 import UserContext from '../context/user';
+
+export default function useUser() {
+    const [activeUser, setActiveUser] = useState({});
+    const { user } = useContext(UserContext);
+    
+}
