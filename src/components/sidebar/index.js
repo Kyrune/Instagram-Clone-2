2fcf4bda0fdaa4847b9e0af3d6f7faf78ce1d033
Kyrune <jn-01@centurytel.net>
2021-06-08 11:19:22 -0500

    Add comment

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -3,6 +3,7 @@ import User from './user';
 import Suggestions from './suggestions';
 
 export default function Sidebar() {
+    // Ensures only users NOT being followed by User show up in Suggestions
     const { 
         user: { fullName, username, userId } 
     } = useUser();
@@ -10,7 +11,7 @@ export default function Sidebar() {
     // console.log('user', user);
     return (
         <div className="p-4">
-            <User username={username} fullName={fullName} />
+            <User usename={username} fullName={fullName} />
             <Suggestions userId={userId} />
         </div>
     );
