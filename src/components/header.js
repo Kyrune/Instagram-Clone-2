4ecc5bf6b040ce2989dc7742e19af8f3408800f7
Kyrune <jn-01@centurytel.net>
2021-06-08 10:13:08 -0500

    Move profile pic outside Sign Out button

--- src/components/header.js
+++ src/components/header.js
@@ -69,16 +69,16 @@ export default function Header() {
                                         d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                                         />
                                     </svg>
-                                    <div className="flex items-center cursor-pointer">
-                                        <Link to={`/p/${user.displayName}`}>
-                                            <img
-                                                className="rounded-full h-8 w-8 flex"
-                                                src={`/images/avatars/${user.displayName}.jpg`}
-                                                alt={`${user.displayName} profile`}
-                                            />
-                                        </Link>
-                                    </div>
                                 </button>
+                                <div className="flex items-center cursor-pointer">
+                                    <Link to={`/p/${user.displayName}`}>
+                                        <img
+                                            className="rounded-full h-8 w-8 flex"
+                                            src={`/images/avatars/${user.displayName}.jpg`}
+                                            alt={`${user.displayName} profile`}
+                                        />
+                                    </Link>
+                                </div>
                             </>
                         ) : (
                             <>
