4da2c129bcb7c4f7bcf30baa78ac22d6fc939475
Kyrune <jn-01@centurytel.net>
2021-06-10 13:02:14 -0500

    Import PropTypes, Link, & Skeleton

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,4 +1,8 @@
-export default function User() {
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Skeleton from 'react-loading-skeleton';
+
+export default function User({ username, fullName }) {
     return (
         <p>I am a user!</p>
     )
