953210124818597b965e84c3a2c6d6badff753d6
Kyrune <jn-01@centurytel.net>
2021-06-10 13:24:15 -0500

    Import memo & add Skeleton for username & fullName

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,14 +1,18 @@
+import { memo } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 
-export default function User({ username, fullName }) {
-    return (
-        <p>I am a user!</p>
-    )
-}
+const User = ({ username, fullName }) =>
+    !username || !fullName ? (
+        <Skeleton count={1} height={61} />
+    ) : (
+ 
+    );
+
+export default User;
 
 User.propTypes = {
-    username: PropTypes.string,
-    fullName: PropTypes.string
+    username: PropTypes.string.isRequired,
+    fullName: PropTypes.string.isRequired
 };
