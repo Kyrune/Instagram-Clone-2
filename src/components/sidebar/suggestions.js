0e92b11daa244afb84361ecc38d9028cba0561a2
Kyrune <jn-01@centurytel.net>
2021-06-14 11:55:25 -0500

    Add JSX for profiles suggestions

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -7,7 +7,13 @@ export default function Suggestions({ userId }) {
 
     return !profiles ? (
         <Skeleton count={10} height={150} className="mt-5" /> 
-    );
+    ) : profiles.length > 0 ? (
+        <div className="rounded flex flex-col">
+            <div className="text-sm flex items-center align-items justify-between mb-2">
+                <p className="font-bold text-gray-base">Suggestions for you</p>
+            </div>
+        </div>
+    ) : null;
 }
 
 Suggestions.propTypes = {
