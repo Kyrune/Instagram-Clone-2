a8bc7eaed3ff0c3b02314ebe23fb6fc6d58cbc84
Kyrune <jn-01@centurytel.net>
2021-06-14 11:58:16 -0500

    Create useEffect that changes on userId

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -5,8 +5,19 @@ import PropTypes from 'prop-types';
 export default function Suggestions({ userId }) {
     const [profiles, setProfiles] = useState(null);
 
+    // Get the suggested profiles
+    // Change on userId
+    useEffect(() => {
+        async function suggestedProfiles() {
+            const response = await getSuggestProfiles(userId);
+            setProfiles(response);
+        }
+        console.log('userId', userId);
+    }, [userId]);
+
     return !profiles ? (
         <Skeleton count={10} height={150} className="mt-5" /> 
+        // Nested turnery - try to avoid when possible
     ) : profiles.length > 0 ? (
         <div className="rounded flex flex-col">
             <div className="text-sm flex items-center align-items justify-between mb-2">
