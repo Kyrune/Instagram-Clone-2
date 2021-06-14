21edcca0495c687746608e4170404917632cbeaf
Kyrune <jn-01@centurytel.net>
2021-06-14 11:40:56 -0500

    Add profiles useState

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 
 export default function Suggestions() {
+    const [profiles, setProfiles] = useState(null);
+
     return (
         <p>I am a suggestion!</p>
     )
