121705cb3484517aced2f212da2e39282ddc683b
Kyrune <jn-01@centurytel.net>
2021-06-14 11:43:55 -0500

    Add userId for PropTypes

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-export default function Suggestions() {
+export default function Suggestions({ userId }) {
     const [profiles, setProfiles] = useState(null);
 
     return (
         <p>I am a suggestion!</p>
-    )
+    );
 }
+
+Suggestions.propTypes = {
+    userId: PropTypes.string
+};
