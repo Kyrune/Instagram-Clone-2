cc8c63d3952bb0d5963346311a1cf14bc5e7aae4
Kyrune <jn-01@centurytel.net>
2021-06-14 11:50:33 -0500

    Import Skeleton

--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react';
+import Skeleton from 'react-loading-skeleton';
 import PropTypes from 'prop-types';
 
 export default function Suggestions({ userId }) {
     const [profiles, setProfiles] = useState(null);
 
-    return (
-        <p>I am a suggestion!</p>
+    return !profiles ? (
+        <Skeleton count={10} height={150} className="mt-5" /> 
     );
 }
 
