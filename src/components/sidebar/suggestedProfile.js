d08094b6c705db5ec3f96381507b69e258367362
Kyrune <jn-01@centurytel.net>
2021-06-15 12:43:53 -0500

    Import PropTypes

--- src/components/sidebar/suggestedProfile.js
+++ src/components/sidebar/suggestedProfile.js
@@ -1,3 +1,12 @@
+import PropTypes from 'prop-types';
+
 export default function SuggestedProfile({ userDocId, username, profileId, userId }) {
     return <p>I am a suggested profile {username}</p>
+}
+
+SuggestedProfile.propTypes = {
+    userDocId: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    profileId: PropTypes.string.isRequired,
+    userId: PropTypes.string.isRequired
 }
