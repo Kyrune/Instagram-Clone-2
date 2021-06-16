57bbc116ceb8a6bc9802d6ddb59fa57ec9a964d3
Kyrune <jn-01@centurytel.net>
2021-06-16 09:37:38 -0500

    Create updateLoggedInUserFollowing async function

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -44,3 +44,19 @@ export async function getSuggestedProfiles(userId, following) {
             .map((user) => ({ ...user.data(), docId: user.id }))
             .filter((profile) => profile.userId !== userId && !following.includes(profile.userId));
 }
+
+export async function updateLoggedInUserFollowing(
+    loggedInUserDocId, 
+    profileId, 
+    isFollowingProfile 
+    ) {
+    return firebase
+        .firestore()
+        .collection('users')
+        .doc(loggedInUserDocId)
+        .update({
+            following: isFollowingProfile
+                ? FieldValue.arrayRemove(profileId)
+                : FieldValue.arrayUnion(profileId)
+        });
+}
