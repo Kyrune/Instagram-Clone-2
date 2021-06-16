20372e1f1d7b65e9a7afb7928936b9750d1bae41
Kyrune <jn-01@centurytel.net>
2021-06-16 09:16:05 -0500

    Add profile picture of suggested user to follow

--- src/components/sidebar/suggestedProfile.js
+++ src/components/sidebar/suggestedProfile.js
@@ -4,7 +4,17 @@ import PropTypes from 'prop-types';
 export default function SuggestedProfile({ userDocId, username, profileId, userId }) {
     const [followed, setFollowed] = useState(false);
 
-    return;
+    return !followed ? (
+        <div className="flex flex-row items-center align-items justify-between">
+            <div className="flex items-center justify-between">
+                <img
+                    className="rounded-full w-8 flex mr-3"
+                    src={`/images/avatars/${username}.jpg`}
+                    alt=""
+                />
+            </div>
+        </div>
+    ) : null;
 }
 
 SuggestedProfile.propTypes = {
