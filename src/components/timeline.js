af00ea9abc8c8062b9d12819174051fd7d79023e
Kyrune <jn-01@centurytel.net>
2021-06-17 11:04:15 -0500

    Import Skeleton

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,5 +1,7 @@
+import Skeleton from 'react-loading-skeleton';
 
 export default function Timeline() {
+
     return (
         <div className="container col-span-2">
             <p>I am the timeline</p>
