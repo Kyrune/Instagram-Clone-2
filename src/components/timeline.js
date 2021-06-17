06671e3aa5e093ed397f9b67aac2fafb14ded89b
Kyrune <jn-01@centurytel.net>
2021-06-17 11:39:14 -0500

    Comment out console.log for photos

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -4,6 +4,8 @@ import usePhotos from '../hooks/use-photos';
 export default function Timeline() {
     // Get the logged in user's photo
     const { photos } = usePhotos();
+
+    // console.log('photos', photos);
     return (
         <div className="container col-span-2">
             <p>I am the timeline</p>
