4de7fc1e915af8df36a19ea45a16d28b24aeb885
Kyrune <jn-01@centurytel.net>
2021-06-17 11:26:08 -0500

    Add useEffect functionality

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -1,11 +1,26 @@
 import { useState, useEffect, useContext } from 'react';
 import UserContext from '../context/user';
+import { getPhotos, getUserByUserId } from '../services/firebase';
 
 export default function usePhotoes() {
     const [photos, setPhotos] = useState(null);
     const {
         user: { uid: userId = '' }
     } = useContext(UserContext);
+
+    useEffect(() => {
+        async function getTimeLinePhotos() {
+            const { following } = await getUserByUserId(userId);
+            let followedUserPhotos = [];
+
+            // Is the user following people?
+            if (following.length > 0) {
+                followedUserPhotos = await getPhotos(userId, following);
+            }
+        }
+
+        console.log(userId);
+    }, []);
 
     return { photos };
 }
