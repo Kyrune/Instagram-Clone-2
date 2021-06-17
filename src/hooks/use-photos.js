7ca87b485380961a5239c48c3c0ebaf6a7c21065
Kyrune <jn-01@centurytel.net>
2021-06-17 11:08:14 -0500

    Add hooks & useContext

--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -1,2 +1,11 @@
 import { useState, useEffect, useContext } from 'react';
 import UserContext from '../context/user';
+
+export default function usePhotoes() {
+    const [photos, setPhotos] = useState(null);
+    const {
+        user: { uid: userId = '' }
+    } = useContext(UserContext);
+
+    return { photos };
+}
