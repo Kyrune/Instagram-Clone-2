f66f8b10b56388c1a7d82cb06bccf80ac9cb27a7
Kyrune <jn-01@centurytel.net>
2021-06-17 11:26:21 -0500

    Add comment

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,7 @@
 import Skeleton from 'react-loading-skeleton';
 
 export default function Timeline() {
-
+    // Get the logged in user's photo
     return (
         <div className="container col-span-2">
             <p>I am the timeline</p>
