94b22777d9e0e60c239009d38d37281e35fc18ed
Kyrune <jn-01@centurytel.net>
2021-06-17 11:32:24 -0500

    Import usePhotos from hooks folder

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,9 @@
 import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
 
 export default function Timeline() {
     // Get the logged in user's photo
+    const { photos } = usePhotos();
     return (
         <div className="container col-span-2">
             <p>I am the timeline</p>
