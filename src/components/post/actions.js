54fde233bb517b5773127859ac41af81fab9760d
Kyrune <jn-01@centurytel.net>
2021-06-21 13:37:26 -0500

    Add constants for hooks

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -2,3 +2,13 @@ import { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import FirebaseContext from '../../context/firebase';
 import UserContext from '../../context/user';
+
+export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) {
+    const {
+        user: { uid: userId = ''}
+    } = useContext(UserContext);
+
+    const [toggleLiked, setToggledLiked] = useState(likedPhoto);
+    const [likes, setLikes] = useState(totalLikes);
+    const { firebase, FieldValue } = useContext(FirebaseContext);
+}
