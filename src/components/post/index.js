f42b9fe675d832aede162a517b31154ac7c7b5d1
Kyrune <jn-01@centurytel.net>
2021-06-21 13:26:48 -0500

    Add Tailwind styling

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -5,7 +5,11 @@ import Header from './header';
 export default function Post({ content }) {
     console.log('content', content);
 
-    return <Header username={content.username} />;
+    return (
+        <div className="rounded col-span-4 border bg-white border-gray-primary mb-16">
+            <Header username={content.username} />
+        </div>
+    );
 }
 
 Post.propTypes = {
