9eae071ed1fcc067d89ad08938e265d6a7843428
Kyrune <jn-01@centurytel.net>
2021-06-21 13:34:20 -0500

    Import useState/useContext, PropTypes, FirebaseContext, & UserContext

---
+++ src/components/post/actions.js
@@ -0,0 +1,4 @@
+import { useState, useContext } from 'react';
+import PropTypes from 'prop-types';
+import FirebaseContext from '../../context/firebase';
+import UserContext from '../../context/user';
