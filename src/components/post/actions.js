ac0eab6cc25a8cc63fb108f08927ba97f8590316
Kyrune <jn-01@centurytel.net>
2021-06-21 13:45:42 -0500

    Add props to propTypes

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -27,3 +27,10 @@ export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) 
             setLikes((likes) => (toggleLiked ? likes - 1 : likes + 1));
     };
 }
+
+Actions.propTypes = {
+    docId: PropTypes.string.isRequired,
+    totalLikes: PropTypes.number.isRequired,
+    likedPhoto: PropTypes.bool.isRequired,
+    handleFocus: PropTypes.func.isRequired
+}
