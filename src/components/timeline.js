a42da38de1ee5039f56442b56f015a67e2cc692c
Kyrune <jn-01@centurytel.net>
2021-06-21 13:01:58 -0500

    Add list of photos

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -8,7 +8,17 @@ export default function Timeline() {
     // console.log('photos', photos);
     return (
         <div className="container col-span-2">
-            <p>I am the timeline</p>
+            {/* Nested turnary */}
+            {!photos ? (
+                // Timestamp ~6:02:00
+                <>
+                    <Skeleton count={1} width={640} height={500} className="mb-5" />
+                </>
+            ) : photos && photos.length > 0 ? (
+                photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+            ) : (
+                <p className="text-center text-2xl">Follow people to see photos!</p>
+            )}
         </div>
     );
 }
